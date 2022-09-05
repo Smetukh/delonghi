93e5dec35c45d894f79ff3419f28e570ec5c8d71
Alex Smetukh <alex.smetukh@gmail.com>
2022-09-06 00:29:42 +0300

    add token

--- threekit.config.js
+++ threekit.config.js
@@ -1,8 +1,12 @@
 export default {
   credentials: {
     preview: {
-      orgId: process.env.THREEKIT_PREVIEW_ORG_ID,
-      publicToken: process.env.THREEKIT_PREVIEW_PUBLIC_TOKEN,
+      orgId:
+        process.env.THREEKIT_PREVIEW_ORG_ID ||
+        '41317b8e-32f8-4d07-95cf-3786368a003d',
+      publicToken:
+        process.env.THREEKIT_PREVIEW_PUBLIC_TOKEN ||
+        'b146a11a-e717-412d-9659-38155ad5ac85',
     },
     'admin-fts': {
       orgId: process.env.THREEKIT_ADMIN_FTS_ORG_ID,
